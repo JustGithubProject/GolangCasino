968c1f7910ca7b0bab4ec972b6f7114ed9f3b776
JustGithubProject <Kropivadanil@gmail.com>
2024-03-06 09:30:02 +0200

    pattern repository

---
+++ cmd/internal/repositories/user_repository.go
@@ -0,0 +1,37 @@
+package repositories
+
+import (
+	"github.com/JustGithubProject/GolangCasino/cmd/internal/models"
+	"gorm.io/gorm"
+)
+
+type UserRepository struct {
+	db *gorm.DB
+}
+
+// Method to create a new user
+func (ur *UserRepository) CreateUser(user *models.User) error{
+	return ur.db.Create(user).Error
+}
+
+
+// Method to get user by id
+func (ur *UserRepository) GetUserById(id uint) (*models.User, error){
+	var user models.User
+	err := ur.db.First(&user, id).Error
+	if err != nil{
+		return nil, err
+	}
+	return &user, nil
+}
+
+
+func (ur *UserRepository) UpdateUser(user *models.User) error {
+    return ur.db.Save(user).Error
+}
+
+// Метод для удаления пользователя
+func (ur *UserRepository) DeleteUser(user *models.User) error {
+    return ur.db.Delete(user).Error
+}
+
