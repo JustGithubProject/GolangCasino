c85d43db46a67a3da7710bb9fe6014c259c11363
JustGithubProject <Kropivadanil@gmail.com>
2024-03-26 10:07:07 +0200

    после логина пользователю в хедеры будет добавляться token

--- cmd/internal/api/handlers/auth_handler.go
+++ cmd/internal/api/handlers/auth_handler.go
@@ -6,6 +6,7 @@ import (
 	"github.com/JustGithubProject/GolangCasino/cmd/internal/database"
 	"github.com/JustGithubProject/GolangCasino/cmd/internal/repositories"
 	"net/http"
+	"database/sql"
 	"github.com/gin-gonic/gin"
 )
 
@@ -65,16 +66,26 @@ func LoginHandler(c *gin.Context) {
 	db := database.InitDB()
     userRepository := repositories.UserRepository{Db: db}
 	user, err := userRepository.GetUserByEmail(userInput.Email)
-	if err != nil{
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON"})
-		return
-	}
+
+	if err != nil {
+        // Проверяем, что ошибка не связана с отсутствием пользователя
+        if err == sql.ErrNoRows {
+            // Если пользователь не найден, возвращаем ошибку 404
+            c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+            return
+        }
+        // Если возникла другая ошибка, возвращаем ошибку 400
+        c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON"})
+        return
+    }
 	if services.CheckPasswordHash(userInput.Password, user.Password){
 		tokenString, err := services.CreateToken(userInput.Name)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
 			return
 		}
+		// Устанавливаем заголовок Authorization
+		c.Header("Authorization", "Bearer "+tokenString)
 		c.JSON(http.StatusOK, gin.H{"token": tokenString})
 		return
 	}
