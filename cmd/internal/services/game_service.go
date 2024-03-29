0c2bdbe1b1e2bbe6360a4e20fcb90d3445ad4fde
JustGithubProject <Kropivadanil@gmail.com>
2024-03-29 11:15:02 +0200

    Будет честный спин и нет(это все шутка деплоить не буду)

--- cmd/internal/services/game_service.go
+++ cmd/internal/services/game_service.go
@@ -10,9 +10,14 @@ type GameRoulette struct{
 	GameName string
 	Numbers []int
 	Sectors []string
+
+    WeightsForNumbers []int
+    WeightsForSectors []int
 }
 
-
+//////////////////////////////////////////////////////////////////////////////////////////////
+//              METHODS FOR UNFAIRSPINROULETTE   (LOOK DOWN)                                //
+//////////////////////////////////////////////////////////////////////////////////////////////
 func (game *GameRoulette) ChooseRandomNumberByWeight(numbers []int, weights []int) int {
     totalWeight := 0
     for _, weight := range weights {
@@ -53,6 +58,41 @@ func (game *GameRoulette) ChooseRandomSectorByWeight(sectors []string, weights [
     return sectors[len(sectors)-1]
 }
 
+
+func (game *GameRoulette) GenerateRandomNumberByWeight(numbers []int, weights []int) int{
+    return game.ChooseRandomNumberByWeight(numbers, weights)
+}
+
+
+func (game *GameRoulette) GenerateRandomSectorByWeight(sectors []string, weights []int) string{
+    return game.ChooseRandomSectorByWeight(sectors, weights)
+}
+
+func (game *GameRoulette) UnfairSpinRoulette(sector string, guess_number int, bet int) (int, error){
+	if sector != nil{
+		dropped_sector := game.GenerateRandomSectorByWeight(game.Sectors, game.WeightsForSectors)
+		if dropped_sector == sector{
+			prize := bet * 3
+			fmt.Printf("You won %d\n", prize)
+			return prize, nil
+		}
+	}
+	dropped_number := game.GenerateRandomNumberByWeight(game.Numbers, game.WeightsForNumbers)
+	if dropped_number == guess_number{
+		prize := bet * 2
+		fmt.Printf("You won %d\n", prize)
+		return prize, nil
+	}else{
+		fmt.Printf("You lose %d\n", bet)
+		return bet, nil
+	}
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+//              METHODS FOR NORMALSPINROULETTE (LOOK DOWN)                                  //
+//////////////////////////////////////////////////////////////////////////////////////////////
+
 func (game *GameRoulette) ChooseRandomNumber(array []int) int {
     // Generate a random index within the range of the array length
     randomIndex := rand.Intn(len(array))
@@ -72,7 +112,8 @@ func (game *GameRoulette) GenerateRandomSectorFromArray(sectors []string) string
 	return game.ChooseRandomSector(sectors)
 }
 
-func (game *GameRoulette) SpinRoulette(sector string, guess_number int, bet int) (int, error){
+
+func (game *GameRoulette) NormalSpinRoulette(sector string, guess_number int, bet int) (int, error){
 	if sector != nil{
 		dropped_sector := game.GenerateRandomSectorFromArray(game.Sectors)
 		if dropped_sector == sector{
@@ -91,4 +132,5 @@ func (game *GameRoulette) SpinRoulette(sector string, guess_number int, bet int)
 		return bet, nil
 	}
 }
+
 
