9d12ebf4c199c3029ec78124e9faa058ca3cd7be
JustGithubProject <Kropivadanil@gmail.com>
2024-04-02 10:28:34 +0200

    много всего леньЭ

--- cmd/internal/services/general_service.go
+++ cmd/internal/services/general_service.go
@@ -1,9 +1,14 @@
 package services
 
 import (
-	"fmt"
+	"errors"
+	"strings"
 	"math/rand"
 	"time"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
 )
 
 func ShuffleWeights(arr []int) {
@@ -28,4 +33,55 @@ func InitNumbersArray(arr []int, length int){
 	for i := 0; i < 37; i++{
 		arr[i] = i
 	}
+}
+
+
+
+func ValidateToken(c *gin.Context) (uint, error) {
+    authHeader := c.GetHeader("Authorization")
+    if authHeader == "" {
+        return 0, errors.New("Authorization header is missing")
+    }
+
+    tokenString := strings.Split(authHeader, " ")[1]
+    token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+        return []byte("your_secret_key"), nil
+    })
+    if err != nil || !token.Valid {
+        return 0, errors.New("Invalid token")
+    }
+
+    claims, ok := token.Claims.(jwt.MapClaims)
+    if !ok {
+        return 0, errors.New("Invalid token claims")
+    }
+    userIDFloat, ok := claims["user_id"].(float64)
+    if !ok {
+        return 0, errors.New("Invalid user ID in token")
+    }
+    userID := uint(userIDFloat)
+
+    return userID, nil
+}
+
+
+
+// Функция для получения параметров игры из запроса
+func GetGameParams(c *gin.Context) (string, int, float64, string, error) {
+    guessSector := c.PostForm("guess_sector")
+    guessNumber := c.PostForm("guess_number")
+    bet := c.PostForm("bet")
+    gameName := c.PostForm("gameName")
+
+    guessNumberInt, err := strconv.Atoi(guessNumber)
+    if err != nil {
+        return "", 0, 0, "", errors.New("Invalid guess number")
+    }
+
+    betFloat, err := strconv.ParseFloat(bet, 64)
+    if err != nil {
+        return "", 0, 0, "", errors.New("Invalid bet")
+    }
+
+    return guessSector, guessNumberInt, betFloat, gameName, nil
 }
--- cmd/internal/services/user_service.go
+++ cmd/internal/services/user_service.go
@@ -11,11 +11,11 @@ type UserPlayer struct {
 	Balance		float64
 }
 
-func (user *UserPlayer) NormalPlay(guess_number int, bet int, gameName string) float64 {
+func (user *UserPlayer) NormalPlay(guess_sector string, guess_number int, bet float64, gameName string) float64 {
 	user.TypeOfGame.GameName = gameName
 
 	services.InitNumbersArray(user.TypeOfGame.Numbers)
-	money, err := user.TypeOfGame.NormalSpinRoulette(guess_number, bet)
+	money, err := user.TypeOfGame.NormalSpinRoulette(guess_sector, guess_number, bet)
 	user.updateBalance(money, bet)
 	return user.Balance
 }
@@ -29,7 +29,7 @@ func (user *UserPlayer) updateBalance(money float64, bet float64){
 	}
 }
 
-func (user *UserPlayer) UnFairPlay(guess_number int, bet float64, gameName string) float64{
+func (user *UserPlayer) UnFairPlay(guess_sector string, guess_number int, bet float64, gameName string) float64{
 	user.TypeOfGame.GameName = gameName
 	services.InitWeights(user.TypeOfGame.WeightsForNumbers, 37)
 	services.InitWeights(user.TypeOfGame.WeightsForSectors, len(user.TypeOfGame.Sectors))
@@ -40,7 +40,7 @@ func (user *UserPlayer) UnFairPlay(guess_number int, bet float64, gameName strin
 
 
 	services.InitNumbersArray(user.TypeOfGame.Numbers)
-	money, err := user.TypeOfGame.UnfairSpinRoulette(guess_number, bet)
+	money, err := user.TypeOfGame.UnfairSpinRoulette(guess_sector, guess_number, bet)
 	user.updateBalance(money, bet)
 	return user.Balance
 }
