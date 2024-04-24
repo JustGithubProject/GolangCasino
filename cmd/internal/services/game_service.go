d2bf2421a273c9e4c641608382581141b9603609
JustGithubProject <Kropivadanil@gmail.com>
2024-04-24 06:07:13 +0300

    Переношу ту логику, которую я написал для GameRoulette в UserPlayer

--- cmd/internal/services/game_service.go
+++ cmd/internal/services/game_service.go
@@ -7,7 +7,6 @@ import (
 
 
 type GameRoulette struct{
-	GameName string
 	Numbers []int
 	Sectors []string
 
--- cmd/internal/services/user_service.go
+++ cmd/internal/services/user_service.go
@@ -7,15 +7,48 @@ type UserPlayer struct {
 	Balance		float64
 }
 
-func (user *UserPlayer) NormalPlay(guess_sectors []string, guess_numbers []int, bet float64, gameName string) (float64, error) {
-	user.TypeOfGame.GameName = gameName
-
+func (user *UserPlayer) NormalPlay(
+	evenToBets map[string]float64,
+	oddToBets map[string]float64,
+	redToBets map[string]float64,
+	blackToBets map[string]float64,
+	sectorsToBets map[string]float64,
+	numbersToBets map[int]float64,
+	oneToEighteenBets map[string]float64,
+	nineteenToThirtySixBets map[string]float64,
+	first2To1Bets map[string]float64,
+	second2To1Bets map[string]float64,
+	third2To1Bets map[string]float64,
+) (float64, error) {
 	InitNumbersArray(user.TypeOfGame.Numbers)
-	money, err := user.TypeOfGame.NormalSpinRoulette(guess_sectors, guess_numbers, bet)
+	prize, err := user.TypeOfGame.NormalSpinRoulette(
+		evenToBets,
+		oddToBets,
+		redToBets,
+		blackToBets,
+		sectorsToBets,
+		numbersToBets,
+		oneToEighteenBets,
+		nineteenToThirtySixBets,
+		first2To1Bets,
+		second2To1Bets,
+		third2To1Bets,
+	)
 	if err != nil{
 		return 0, &GameError{Message: "Game play error: " + err.Error()}
 	}
-	user.updateBalance(money, bet)
+	totalBet := user.getTotalBet(evenToBets, oddToBets,
+		redToBets,
+		blackToBets,
+		sectorsToBets,
+		numbersToBets,
+		oneToEighteenBets,
+		nineteenToThirtySixBets,
+		first2To1Bets,
+		second2To1Bets,
+		third2To1Bets,
+	)
+	user.updateBalance(prize, totalBet)
 	return user.Balance, nil
 }
 
@@ -26,6 +59,39 @@ func (user *UserPlayer) updateBalance(money float64, bet float64){
 	}else{
 		user.Balance -= money
 	}
+}
+
+func (user *UserPlayer) sumMapValues(bets map[string]float64) float64 {
+    sum := 0.0
+    for _, value := range bets {
+        sum += value
+    }
+    return sum
+}
+
+func (user *UserPlayer) getTotalBet(evenToBets map[string]float64, oddToBets map[string]float64,
+	redToBets map[string]float64, blackToBets map[string]float64,
+	sectorsToBets map[string]float64, numbersToBets map[int]float64,
+	oneToEighteenBets map[string]float64, nineteenToThirtySixBets map[string]float64,
+	first2To1Bets map[string]float64, second2To1Bets map[string]float64,
+	third2To1Bets map[string]float64) float64 {
+
+	var total float64
+
+	// Добавляем суммы ставок из карты ставок для каждого типа
+	total += user.sumMapValues(evenToBets)
+	total += user.sumMapValues(oddToBets)
+	total += user.sumMapValues(redToBets)
+	total += user.sumMapValues(blackToBets)
+	total += user.sumMapValues(sectorsToBets)
+	total += user.sumMapValues(numbersToBets)
+	total += user.sumMapValues(oneToEighteenBets)
+	total += user.sumMapValues(nineteenToThirtySixBets)
+	total += user.sumMapValues(first2To1Bets)
+	total += user.sumMapValues(second2To1Bets)
+	total += user.sumMapValues(third2To1Bets)
+
+	return total
 }
 
 func (user *UserPlayer) UnFairPlay(guess_sector string, guess_number int, bet float64, gameName string) (float64, error){
