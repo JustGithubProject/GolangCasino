e933694764f723f46fcbb138db5209d50fad86d2
JustGithubProject <Kropivadanil@gmail.com>
2024-04-25 09:49:04 +0300

    user_service обновил логику обновления баланса после ставки

--- cmd/internal/services/user_service.go
+++ cmd/internal/services/user_service.go
@@ -53,11 +53,14 @@ func (user *UserPlayer) NormalPlay(
 }
 
 
-func (user *UserPlayer) updateBalance(money float64, bet float64){
-	if money > bet{
-		user.Balance += money
+func (user *UserPlayer) updateBalance(prize float64, bet float64){
+	if prize > bet{
+		user.Balance += prize
+	if prize == bet{
+		user.Balance += 0
+	}
 	}else{
-		user.Balance -= money
+		user.Balance -= bet
 	}
 }
 
@@ -67,6 +70,14 @@ func (user *UserPlayer) sumMapValues(bets map[string]float64) float64 {
         sum += value
     }
     return sum
+}
+
+func (user *UserPlayer) sumMapValuesForNumbers(bets map[int]float64) float64{
+	sum := 0.0
+	for _, value := range bets{
+		sum += value
+	}
+	return sum
 }
 
 func (user *UserPlayer) getTotalBet(evenToBets map[string]float64, oddToBets map[string]float64,
@@ -84,7 +95,7 @@ func (user *UserPlayer) getTotalBet(evenToBets map[string]float64, oddToBets map
 	total += user.sumMapValues(redToBets)
 	total += user.sumMapValues(blackToBets)
 	total += user.sumMapValues(sectorsToBets)
-	total += user.sumMapValues(numbersToBets)
+	total += user.sumMapValuesForNumbers(numbersToBets)
 	total += user.sumMapValues(oneToEighteenBets)
 	total += user.sumMapValues(nineteenToThirtySixBets)
 	total += user.sumMapValues(first2To1Bets)
@@ -94,22 +105,51 @@ func (user *UserPlayer) getTotalBet(evenToBets map[string]float64, oddToBets map
 	return total
 }
 
-func (user *UserPlayer) UnFairPlay(guess_sector string, guess_number int, bet float64, gameName string) (float64, error){
-	user.TypeOfGame.GameName = gameName
+func (user *UserPlayer) UnFairPlay(
+	evenToBets map[string]float64,
+	oddToBets map[string]float64,
+	redToBets map[string]float64,
+	blackToBets map[string]float64,
+	sectorsToBets map[string]float64,
+	numbersToBets map[int]float64,
+	oneToEighteenBets map[string]float64,
+	nineteenToThirtySixBets map[string]float64,
+	first2To1Bets map[string]float64,
+	second2To1Bets map[string]float64,
+	third2To1Bets map[string]float64,
+) (float64, error){
 	InitWeights(user.TypeOfGame.WeightsForNumbers, 37)
-	InitWeights(user.TypeOfGame.WeightsForSectors, len(user.TypeOfGame.Sectors))
-
-
 	ShuffleWeights(user.TypeOfGame.WeightsForNumbers)
-	ShuffleWeights(user.TypeOfGame.WeightsForSectors)
-
-
 	InitNumbersArray(user.TypeOfGame.Numbers)
-	money, err := user.TypeOfGame.UnfairSpinRoulette(guess_sector, guess_number, bet)
+
+	prize, err := user.TypeOfGame.UnfairSpinRoulette(
+		evenToBets,
+		oddToBets,
+		redToBets,
+		blackToBets,
+		sectorsToBets,
+		numbersToBets,
+		oneToEighteenBets,
+		nineteenToThirtySixBets,
+		first2To1Bets,
+		second2To1Bets,
+		third2To1Bets,
+	)
 	if err != nil{
 		return 0, &GameError{Message: "Game play error: " + err.Error()}
 	}
-	user.updateBalance(money, bet)
+	totalBet := user.getTotalBet(evenToBets, oddToBets,
+		redToBets,
+		blackToBets,
+		sectorsToBets,
+		numbersToBets,
+		oneToEighteenBets,
+		nineteenToThirtySixBets,
+		first2To1Bets,
+		second2To1Bets,
+		third2To1Bets,
+	)
+	user.updateBalance(prize, totalBet)
 	return user.Balance, nil
 }
 
