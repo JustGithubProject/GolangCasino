117f45802c61f1e76887ed3549151a277136a911
JustGithubProject <Kropivadanil@gmail.com>
2024-08-13 20:03:07 +0300

    started to write payment history page

--- frontend-casino/src/components/payments/PaymentsHistory.jsx
+++ frontend-casino/src/components/payments/PaymentsHistory.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
+import axios from 'axios';
 
 const PaymentHistoryPage = () => {
+
+    const getListOrdersOrPayments = async () => {
+        try{
+            const token = localStorage.getItem("token");
+            if (!token){
+                // TODO...
+            }
+        }catch(error){
+
+        }
+    }
+
     return (
         <div>
             <h1 align="center">История платежей</h1>
